9702f63b0e1582759bcdb305ce7808bf707db189
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-07 22:48:06 +0700

    set up headere and navbutton

---
+++ components/nav-button.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  href: string;
+  label: string;
+  isActive?: boolean;
+};
+
+export const NavButton = ({ href, label, isActive }: Props) => {
+  return (
+    <Button
+      asChild
+      size="sm"
+      variant="outline"
+      className={cn(
+        "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus:visited:ring-offset-transparent outline-none text-white focus:text-white/30 transition",
+        isActive ? "bg-white/10 text-white" : "bg-transparent",
+      )}
+    >
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
