6d87d511907bb147f34f70f43c9e5ed61577586f
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-08 22:06:27 +0700

    page tsx for home page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Dashboard Page</div>;
+  const { onOpen } = useNewAccount();
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an account</Button>
+    </div>
+  );
 }
