6836d6db46c5c99dfe7e1c49e181525688f96392
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-09 22:59:50 +0700

    column table and page for accounts dir

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { columns } from "./columns";
+import { Loader2, Plus } from "lucide-react";
+
+const AccountPage = () => {
+  const newAccount = useNewAccount();
+  const accountQuery = useGetAccounts();
+  const accounts = accountQuery.data || [];
+
+  if (accountQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm"></Card>
+        <CardHeader>
+          <Skeleton className="h-8 w-48" />
+        </CardHeader>
+        <CardContent>
+          <div className="h-[500px] w-full flex items-center justify-center">
+            <Loader2 className="size-6 text-slate-300 animate-spin" />
+          </div>
+        </CardContent>
+      </div>
+    );
+  }
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">AccountPage</CardTitle>
+          <Button size="sm" onClick={newAccount.onOpen}>
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={accounts}
+            filterKey="name"
+            onDelete={() => {}}
+            disabled={false}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AccountPage;
