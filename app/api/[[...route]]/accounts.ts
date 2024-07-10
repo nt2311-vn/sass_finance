463bdc0568f95c54674fe2993ce27f973f3dbabb
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-10 21:51:10 +0700

    write api for bulk delete accs

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,43 +1,74 @@
 import { Hono } from "hono";
+import { z } from "zod";
+
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
 
 import { db } from "@/db/drizzle";
 import { accounts, insertAccountSchema } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 
 const app = new Hono()
-	.get("/", clerkMiddleware(), async (c) => {
-		const auth = getAuth(c);
-		if (!auth?.userId) {
-			return c.json({ error: "Unauthorized" }, 401);
-		}
-		const data = await db
-			.select({ id: accounts.id, name: accounts.name })
-			.from(accounts)
-			.where(eq(accounts.userId, auth.userId));
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+    const data = await db
+      .select({ id: accounts.id, name: accounts.name })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
 
-		return c.json({ data });
-	})
-	.post(
-		"/",
-		clerkMiddleware(),
-		zValidator("json", insertAccountSchema.pick({ name: true })),
-		async (c) => {
-			const auth = getAuth(c);
-			const values = c.req.valid("json");
+    return c.json({ data });
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
 
-			if (!auth?.userId) {
-				return c.json({ error: "Unauthorized" }, 401);
-			}
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
-			const [data] = await db
-				.insert(accounts)
-				.values({ id: createId(), userId: auth?.userId, ...values })
-				.returning();
-			return c.json({ data });
-		},
-	);
+      const [data] = await db
+        .insert(accounts)
+        .values({ id: createId(), userId: auth?.userId, ...values })
+        .returning();
+      return c.json({ data });
+    },
+  )
+  .post(
+    "/bulk-delete",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.object({
+        ids: z.array(z.string()),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+      const data = await db
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            inArray(accounts.id, values.ids),
+          ),
+        )
+        .returning({ id: accounts.id });
+
+      return c.json({ data });
+    },
+  );
 
 export default app;
